ae8e8c44fe611c3e1a07dd4269ae1e06a51fb247
EC2 Default User <ec2-user@ip-10-0-0-64.ec2.internal>
2020-12-10 06:57:04 +0000

    Initial commit

---
+++ read_and_publish.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import dht11
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from time import sleep
+from datetime import date, datetime
+ 
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+ 
+# AWS IoT certificate based connection
+myMQTTClient = AWSIoTMQTTClient("123afhlss456")
+myMQTTClient.configureEndpoint("a167lgi887r2fi-ats.iot.us-east-1.amazonaws.com", 8883)
+myMQTTClient.configureCredentials("/home/ec2-user/root-CA.crt", "/home/ec2-user/project-private.pem.key", "/home/ec2-user/project-certificate.pem.crt")
+myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+ 
+#connect and publish
+myMQTTClient.connect()
+myMQTTClient.publish("thing01/info", "connected", 0)
+ 
+#loop and publish sensor reading
+
+while 1:
+    now = datetime.utcnow()
+    now_str = now.strftime('%Y-%m-%dT%H:%M:%SZ') #e.g. 2016-04-18T06:12:25.877Z
+    instance = dht11.DHT11(pin = 4) #BCM GPIO04
+    result = instance.read()
+    if result.is_valid():
+        payload = '{ "timestamp": "' + now_str + '","temperature": ' + str(result.temperature) + ',"humidity": '+ str(result.humidity) + ' }'
+        print payload
+        myMQTTClient.publish("thing01/data", payload, 0)
+        sleep(4)
+    else:
+        print (".")
+        sleep(1)
